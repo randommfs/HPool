f3adeb977ef5810f4c903c3b5c35fb59a441088f
HyperWinX <artmsmth@gmail.com>
2024-06-06 15:15:15 +0400

    First passed test

--- cpp/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hyperwin/d/Projects/HPool/cpp
-# Build directory: /home/hyperwin/d/Projects/HPool/cpp/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[runTests]=] "runTests")
-set_tests_properties([=[runTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/hyperwin/d/Projects/HPool/cpp/CMakeLists.txt;12;add_test;/home/hyperwin/d/Projects/HPool/cpp/CMakeLists.txt;0;")
--- cpp/include/hpool.hpp
+++ cpp/include/hpool.hpp
@@ -3,6 +3,12 @@ namespace hpool{
     class pool_ptr;
 
     template<typename T>
+    struct Element{
+        T object;
+        bool used;
+    };
+
+    template<typename T>
     class IPool{
     public:
         virtual pool_ptr<T> allocate() = 0;
@@ -15,17 +21,12 @@ namespace hpool{
     template<typename T>
     class pool_ptr{
     private:
-        struct Element{
-            T object;
-            bool used;
-        };
-
-        Element* ptr;
+        Element<T>* ptr;
         IPool<T>& pool;
         uint32_t index;
         bool valid;
     public:
-        pool_ptr(T* ptr, IPool<T>& pool, uint32_t index) :
+        pool_ptr(Element<T>* ptr, IPool<T>& pool, uint32_t index) :
             ptr(ptr),
             pool(pool),
             index(index),
@@ -47,12 +48,7 @@ namespace hpool{
     template<typename T>
     class HPool : IPool<T>{
     private:
-        struct Element{
-            T object;
-            bool used;
-        };
-
-        Element* pool;
+        Element<T>* pool;
         uint32_t total_elements;
         uint32_t allocated_elements;
         uint32_t nearest_free_block;
@@ -66,7 +62,7 @@ namespace hpool{
             nearest_free_block(0),
             has_free_blocks(true){
 
-            pool = static_cast<Element*>(calloc(sizeof(Element), element_count));
+            pool = static_cast<Element<T>*>(calloc(sizeof(Element<T>), element_count));
             if (!pool)
                 throw std::runtime_error("Failed to allocate memory");
         }
@@ -75,5 +71,93 @@ namespace hpool{
 
         uint32_t get_total_elements() override;
         uint32_t get_allocated_elements() override;
+
+        ~HPool();
     };
+}
+
+template<typename T>
+T& hpool::pool_ptr<T>::operator*() {
+    if (!ptr->used)
+        throw std::runtime_error("use-after-free detected!");
+    return &ptr->object;
+}
+
+template<typename T>
+T* hpool::pool_ptr<T>::operator->() {
+    if (!ptr->used)
+        throw std::runtime_error("use-after-free detected!");
+    return &ptr->object;
+}
+
+template<typename T>
+T* hpool::pool_ptr<T>::get() {
+    if (!ptr->used)
+        throw std::runtime_error("use-after-free detected!");
+    return &ptr->object;
+}
+
+template<typename T>
+void hpool::pool_ptr<T>::free(){
+    pool.free(*this);
+}
+
+template<typename T>
+uint32_t hpool::pool_ptr<T>::_idx() {
+    return index;
+}
+
+template<typename T>
+void hpool::pool_ptr<T>::_validate() {
+    valid = ptr->used;
+}
+
+template<typename T>
+bool hpool::pool_ptr<T>::is_valid() {
+    return valid;
+}
+
+template<typename T>
+hpool::pool_ptr<T>::~pool_ptr(){
+    pool.free(*this);
+}
+
+template<typename T>
+uint32_t hpool::HPool<T>::find_nearest_free_block() {
+    for (uint32_t i = 0; i < total_elements; ++i)
+        if (!pool[i].used) return i;
+    has_free_blocks = false;
+    return 0;
+}
+
+template<typename T>
+hpool::pool_ptr<T> hpool::HPool<T>::allocate() {
+    uint32_t index = nearest_free_block;
+    pool[index].used = true;
+    ++allocated_elements;
+    nearest_free_block = find_nearest_free_block();
+    return pool_ptr<T>(&pool[index], *this, index);
+}
+
+template<typename T>
+void hpool::HPool<T>::free(hpool::pool_ptr<T>& ptr){
+    pool[ptr._idx()].used = false;
+    --allocated_elements;
+    if (ptr._idx() < nearest_free_block)
+        nearest_free_block = ptr._idx();
+}
+
+template<typename T>
+uint32_t hpool::HPool<T>::get_total_elements() {
+    return total_elements;
+}
+
+template<typename T>
+uint32_t hpool::HPool<T>::get_allocated_elements() {
+    return allocated_elements;
+}
+
+template<typename T>
+hpool::HPool<T>::~HPool(){
+    std::free(pool);
 }
