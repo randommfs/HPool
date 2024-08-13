7e2ec8e6a781aa73347c447bba282f94180622e8
HyperWinX <artmsmth@gmail.com>
2024-08-13 19:35:20 +0400

    Added test for custom element type

--- test/tests.cpp
+++ test/tests.cpp
@@ -13,18 +13,10 @@ TEST(HPOOL, PTR_FREE) {
   EXPECT_EQ(pool.get_allocated_elements(), 0);
 }
 
-TEST(HPOOL, CUSTOM_T_SIZE) {
-  hpool::HPool<int64_t, 32> pool(32);
-  ASSERT_EQ(pool.get_total_elements(), 32);
-
-  auto ptr = pool.allocate();
-  EXPECT_EQ(pool.get_allocated_elements(), 1);
-
-  *ptr = 5;
-  pool.free(ptr);
-  EXPECT_EQ(pool.get_allocated_elements(), 0);
-}
-
 TEST(HPOOL, ELEMENT_SIZE) {
   ASSERT_EQ(sizeof(hpool::Element<size_t, sizeof(size_t)>), 9);
+}
+
+TEST(HPOOL, CUSTOM_SIZE) {
+  ASSERT_EQ(sizeof(hpool::Element<size_t, 32>), 33);
 }
